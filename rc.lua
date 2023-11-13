8533649cd42e212139b119663087c6f5a68eb34f
ysl2 <www.songli.yu@gmail.com>
2023-11-13 11:41:23 +0800

    Bar transparent

--- rc.lua
+++ rc.lua
@@ -207,7 +207,7 @@ awful.screen.connect_for_each_screen(function(s)
     }
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
+    s.mywibox = awful.wibar({ position = "top", screen = s, bg = beautiful.bg_normal .. "b0" })
 
     -- Add widgets to the wibox
     s.mywibox:setup {
